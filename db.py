1a6721bdf05d3561d7d094360251cba0dcc02db4
DaNiel11011 <danielvargaszamora18@gmail.com>
2026-06-17 22:05:00 -0400

    Adaptar conexión para Render

--- db.py
+++ db.py
@@ -2,12 +2,18 @@ import psycopg2
 import os
 
 def conectar():
+    database_url = os.getenv("DATABASE_URL")
+
+    # Si estamos en Render o en otro servidor
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    # Si estamos trabajando en la PC local
     os.environ["PGPASSFILE"] = "NUL"
+
     return psycopg2.connect(
         host="localhost",
         database="colegio_db",
         user="postgres",
         password="22022005"
     )
-
-# en password se debe colocar la contraseña del usuario postgres, si es diferente a la que se muestra.
